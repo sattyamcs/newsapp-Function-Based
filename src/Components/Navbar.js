f627dbd59b902d1ffbf96446a5e5269c9ee07c99
sattyam <sattyamthakur02@gmail.com>
2022-04-21 11:44:23 +0530

    Some Changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar=()=>{
             <div>
                 <nav className="navbar  navbar-expand-lg  navbar-light sticky" style={{backgroundColor: "#e3f2fd"}}>
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/general">NewsApp</Link>
+                        <Link className="navbar-brand" to="/general" style={{fontWeight:"500"}}>NEWSAPP</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
